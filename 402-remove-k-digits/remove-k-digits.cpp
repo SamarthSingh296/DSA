fa59d6d0360527e2e535c8c3e81b610cf434cd7d
SamarthSingh296 <153537590+SamarthSingh296@users.noreply.github.com>
2025-11-15 23:24:57 +0530

    Time: 7 ms (25.45%) | Memory: 11.1 MB (90.83%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+           string stack = "";
+
+    for (char c : num) {
+        // Greedily remove previous larger digits
+        while (!stack.empty() && k > 0 && stack.back() > c) {
+            stack.pop_back();
+            k--;
+        }
+        stack.push_back(c);
+    }
+
+    // If k still remains, remove from the end
+    while (k > 0 && !stack.empty()) {
+        stack.pop_back();
+        k--;
+    }
+
+    // Remove leading zeros
+    int i = 0;
+    while (i < stack.size() && stack[i] == '0') i++;
+
+    string result = stack.substr(i);
+    return result.empty() ? "0" : result;
+}
+        
+    
+};
