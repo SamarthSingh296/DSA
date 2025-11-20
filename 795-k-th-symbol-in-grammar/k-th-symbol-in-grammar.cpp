5b3b35821ab473e3378b0be57c1acd2a0d7b80c7
SamarthSingh296 <153537590+SamarthSingh296@users.noreply.github.com>
2025-11-20 15:50:29 +0530

    Time: 2 ms (1.51%) | Memory: 12.6 MB (1.17%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int kthGrammar(int n, int k) {
+    vector<int>dp(n+1,-1);
     int len=pow(2,n-1);
     int mid=len/2;
+    if(dp[n]!=-1)return dp[n];
     if(n==1&&k==1)return 0;
-    if(k<=mid)return kthGrammar(n-1,k);
-    else return 1-kthGrammar(n-1,k-mid);
+    if(k<=mid)dp[n]= kthGrammar(n-1,k);
+    else dp[n]= 1-kthGrammar(n-1,k-mid);
+    return dp[n];
 
         
         
