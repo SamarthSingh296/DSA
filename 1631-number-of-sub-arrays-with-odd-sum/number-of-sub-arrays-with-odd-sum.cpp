cda0710fbbd74f3fdf6c3c529d54a46353140128
Samarth Singh <153537590+SamarthSingh296@users.noreply.github.com>
2025-12-08 10:48:48 +0530

    Time: 11 ms (20.48%) | Memory: 112 MB (77.70%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -17,13 +17,11 @@ int M=1e9+7;
         int oddcnt=0;
         int cnt=0;
         int n=arr.size();
-        vector<int>prefix(n,0);
-        prefix[0]=arr[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+arr[i];
-        }
+        int sum=0;
         for(int i=0;i<n;i++){
-            if(prefix[i]%2==0){
+        
+            sum+=arr[i];
+            if(sum%2==0){
                 cnt=(cnt+oddcnt)%M;
                 evencnt++;
             }
