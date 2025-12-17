cc1b993054d9b502336caf6c826ff9529e9bf8e8
Samarth Singh <153537590+SamarthSingh296@users.noreply.github.com>
2025-12-17 18:55:37 +0530

    Time: 1 ms (43.43%) | Memory: 10 MB (40.69%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
-    void generateSubsets(int index, vector<int>& nums, vector<int>& current, vector<vector<int>>& result) {
-        if (index == nums.size()) {
-        result.push_back(current);
-        return;
-    }
+//     void generateSubsets(int index, vector<int>& nums, vector<int>& current, vector<vector<int>>& result) {
+//         if (index == nums.size()) {
+//         result.push_back(current);
+//         return;
+//     }
 
-    generateSubsets(index + 1, nums, current, result);
+//     generateSubsets(index + 1, nums, current, result);
 
-    current.push_back(nums[index]);
-    generateSubsets(index + 1, nums, current, result);
+//     current.push_back(nums[index]);
+//     generateSubsets(index + 1, nums, current, result);
 
-    current.pop_back();
-}
+//     current.pop_back();
+// }
 
 vector<vector<int>> subsets(vector<int>& nums) {
-    vector<vector<int>> result;
-    vector<int> current;
-    generateSubsets(0, nums, current, result);
-    return result;
+    // vector<vector<int>> result;
+    // vector<int> current;
+    // generateSubsets(0, nums, current, result);
+    // return result;
+    int n=nums.size();
+    int sub=1<<n;
+    vector<vector<int>>res;
+    for(int num=0;num<sub;num++){
+        vector<int>list;
+        for(int j=0;j<n;j++){
+            if((num&(1<<j)))list.push_back(nums[j]);
+        }
+        res.push_back(list);
+    }
+    return res;
         
     }
 };
