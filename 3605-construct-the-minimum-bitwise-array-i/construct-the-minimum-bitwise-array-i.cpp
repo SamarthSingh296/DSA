d2941eb6386da91ad167c86918d2cce60b438f5a
Samarth Singh <153537590+SamarthSingh296@users.noreply.github.com>
2026-01-22 06:19:59 +0530

    Time: 0 ms (100.00%) | Memory: 25.4 MB (9.47%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
@@ -1,21 +1,52 @@
 class Solution {
 public:
+
     vector<int> minBitwiseArray(vector<int>& nums) {
+
         vector<int>result;
         int n=nums.size();
+
         for(int i=0;i<n;i++){
+            if(nums[i]==2){
+                result.push_back(-1);
+                continue;}
             bool found=false;
-            for(int j=0;j<nums[i];j++){
-                if((j|(j+1))==nums[i]){
-                    result.push_back(j);
+            for(int j=0;j<32;j++){
+                if((nums[i]&(1<<j))!=0)continue;
+                //make  it 0;
+                else {
+                    int x=nums[i]^(1<<(j-1));
+                    result.push_back(x);
                     found=true;
                     break;
 
-                }
+
+
             }
-            if(!found)result.push_back(-1);
+        }
         }
         return result;
+
+
+
+
+
+
+        // vector<int>result;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     bool found=false;
+        //     for(int j=0;j<nums[i];j++){
+        //         if((j|(j+1))==nums[i]){
+        //             result.push_back(j);
+        //             found=true;
+        //             break;
+
+        //         }
+        //     }
+        //     if(!found)result.push_back(-1);
+        // }
+        // return result;
         
     }
 };
