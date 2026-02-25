211724876ffb6851ace73bbb059aa44f85d37b12
Samarth Singh <153537590+SamarthSingh296@users.noreply.github.com>
2026-02-25 17:57:26 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (40.33%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -22,13 +22,11 @@ public:
             for(int i=0;i<size;i++){
                  TreeNode* temp=q.front();
                  q.pop();
-
+                 if(i==size-1)res.push_back(temp->val);
                  if(temp->left)q.push(temp->left);
                  if(temp->right)q.push(temp->right);
-                 put=temp;
 
             }
-            res.push_back(put->val);
 
 
 
