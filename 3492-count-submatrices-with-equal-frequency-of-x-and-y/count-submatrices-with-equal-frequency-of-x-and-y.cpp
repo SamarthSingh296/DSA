71e0e62ce4b2a4e36c6bbece081a63281e0e9ee6
Samarth Singh <153537590+SamarthSingh296@users.noreply.github.com>
2026-03-19 17:50:03 +0530

    Time: 50 ms (70.73%) | Memory: 129.4 MB (69.92%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
     int numberOfSubmatrices(vector<vector<char>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<pair<int,int>>>mat(m,vector<pair<int,int>>(n,{0,0}));
-        for(int i=0;i<m;i++){
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<pair<int, int>>> mat(m,
+                                           vector<pair<int, int>>(n, {0, 0}));
+        for(int  i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int x=grid[i][j]=='X';
-                int y=grid[i][j]=='Y';
-                mat[i][j]={x,y};
-                if(i>0){
-                    mat[i][j].first+=mat[i-1][j].first;
-                     mat[i][j].second+=mat[i-1][j].second;
-                }
-
-                if(j>0){
-                    mat[i][j].first+=mat[i][j-1].first;
-                    mat[i][j].second+=mat[i][j-1].second;
-
-                }
-                if(i>0&&j>0){
-                    mat[i][j].first-=mat[i-1][j-1].first;
-                    mat[i][j].second-=mat[i-1][j-1].second;
-                }
-
-                
+                mat[i][j]={grid[i][j]=='X',grid[i][j]=='Y'};
             }
         }
-        int ans=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int cntx=mat[i][j].first;
-                int cnty=mat[i][j].second;
-                if(cntx==cnty&&cntx>0)ans++;
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                    mat[i][j].first += mat[i - 1][j].first;
+                    mat[i][j].second += mat[i - 1][j].second;
+                }
+            
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                    mat[i][j].first += mat[i][j - 1].first;
+                    mat[i][j].second += mat[i][j - 1].second;
+                }
+                // if(i>0&&j>0){
+                //     mat[i][j].first-=mat[i-1][j-1].first;
+                //     mat[i][j].second-=mat[i-1][j-1].second;
+                // }
+            
+        }
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int cntx = mat[i][j].first;
+                int cnty = mat[i][j].second;
+                if (cntx == cnty && cntx > 0)
+                    ans++;
             }
         }
         return ans;
-        
-
     }
 };
