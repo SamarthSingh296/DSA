aa4a1a00b84aeaeb41c3d23b4c590e5c0dc9c23d
Samarth Singh <153537590+SamarthSingh296@users.noreply.github.com>
2026-04-01 14:26:34 +0530

    Time: 64 ms (36.73%) | Memory: 219.4 MB (24.95%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n=positions.size();
+        struct Robot{
+            int pos;
+            int health;
+           char dir;
+           int idx;
+
+        };
+        vector<Robot>robots;
+        for(int i=0;i<n;i++){
+            robots.push_back({positions[i],healths[i],directions[i],i});
+        }
+        sort(robots.begin(),robots.end(),[](auto &a,auto&b){
+            return a.pos<b.pos;
+        });
+        stack<pair<int,int>>st;
+        for(int i=0;i<n;i++){
+            if(robots[i].dir=='R')st.push({robots[i].pos,i});
+            else{
+
+                while(!st.empty()&&robots[i].health>0){
+                    int j=st.top().second;
+                    if(robots[i].health>robots[j].health){
+                        robots[i].health--;
+                        robots[j].health=0;
+                        st.pop();
+                    }
+                    else if(robots[i].health<robots[j].health){
+                        robots[j].health--;
+                        robots[i].health=0;
+                        break;
+                    }
+                     else {
+                        robots[j].health=0;
+                        robots[i].health=0;
+                        st.pop();
+                        break;
+                    }
+                    if(robots[i].health==0)break;
+                    }
+                }
+            }
+        
+        vector<pair<int,int>>temp;
+        for(auto &it:robots){
+            if(it.health>0){
+                temp.push_back({it.idx,it.health});
+            }
+
+        }
+        sort(temp.begin(),temp.end());
+        vector<int>res;
+        for(auto it:temp){
+            res.push_back(it.second);
+        }
+        return res;
+
+
+
+        
+    }
+};
