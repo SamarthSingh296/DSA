67ad2b13cba5158e0113d7e98f156e6ecd583703
Samarth Singh <153537590+SamarthSingh296@users.noreply.github.com>
2026-04-10 22:49:46 +0530

    Time: 3 ms (71.85%) | Memory: 29.7 MB (47.69%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
@@ -3,15 +3,18 @@ public:
     int minimumDistance(vector<int>& nums) {
         int  n=nums.size();
         int ans=INT_MAX;
+        unordered_map<int,vector<int>>mpp;
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    if(nums[i]==nums[j]&&nums[j]==nums[k]){
-                        int dist = abs(i - j) + abs(j - k) + abs(k - i);
-                        ans=min(dist,ans);
-                    }
+            mpp[nums[i]].push_back(i);
+        }
+        for(auto &[val,indices]:mpp){
+            if(indices.size()<3)continue;
+            for(int i=0;i<indices.size()-2;i++){
+                if(ans>2*(indices[i+2]-indices[i])){
+                    ans=2*(indices[i+2]-indices[i]);
                 }
-            }
+
+        }
         }
         return ans==INT_MAX?-1:ans;
         
