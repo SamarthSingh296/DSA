5d4718cc3637d886f36b0e30306a2f77b56f3d46
Samarth Singh <153537590+SamarthSingh296@users.noreply.github.com>
2026-05-01 11:14:58 +0530

    Time: 410 ms (55.52%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,3 +1,2 @@
--- Write your PostgreSQL query statement below
-select  distinct author_id as id from Views
-where author_id=viewer_id ;
+# Write your MySQL query statement below
+select distinct author_id as id from views where  author_id=viewer_id order by id asc;
